8420208601deb58109a9982bee6ae3d1b6669848
Raihan Ahmed <raihan.ahmed117@gmail.com>
2022-03-14 19:55:26 -0500

    Completed CSVFiles module; Program unable to find test file for some reason.

--- FilesAndJson/CSVFiles/src/Database.java
+++ FilesAndJson/CSVFiles/src/Database.java
@@ -27,15 +27,31 @@ public class Database {
         this.data = data;
     }
 
-    public Database(String contents) {
-/* TODO 
-This constructor should take the contents of a CSV file and initialize the memember variables of the Database class.
+    /* TODO
+This constructor should take the contents of a CSV file and initialize the member variables of the Database class.
 */
+    public Database(String contents) {
+        String[] table = contents.split("\n");
+
+        this.colNames = table[0].split(" ");
+        this.numRows = table.length - 1;
+
+        for (int i = 1; i < table.length; i++) {
+            data[i-1] = table[i].split(" ");
+        }
+
     }
 
-    public String getValue(String columnName,int row){
-/* TODO */
-This method should return the data contained on row "row" and the column matching  @columname
+    /* TODO
+This method should return the data contained on row "row" and the column matching  @columname */
+    public String getValue(String columnName, int row){
+        int col = 0;
+
+        for (int i = 0; i < colNames.length; i++) {
+            if (colNames[i].equals(columnName)) col = i;
+        }
+
+        return data[row][col];
     }
 
 }
--- FilesAndJson/CSVFiles/src/Task.java
+++ FilesAndJson/CSVFiles/src/Task.java
@@ -3,24 +3,38 @@ import java.io.*;
 public class Task {
     public static void main(String[] ar) {
         Task t = new Task();
-        Database d = t.readCSV("./resources/iris-data.csv");
+        Database d = t.readCSV("./test/iris-data.csv");
     }
 
     public Database readCSV(String filename) {
         String contents = "";
-        try (InputStream in = /* TODO.
-Change what needs to go here to start reading "filename" */){
-            byte[] buffer = /* TODO 
-Create a byte array of about 1024 bytes or however many bytes you want. */;
+
+        /* TODO
+Change what needs to go here to start reading "filename" */
+        try (InputStream in = new BufferedInputStream(new FileInputStream(filename));
+        OutputStream out = new BufferedOutputStream(new FileOutputStream(contents))) {
+            /* TODO
+Create a byte array of about 1024 bytes or however many bytes you want. */
+            byte[] buffer = new byte[1024];
             int lengthRead;
             /* TODO 
 Create a while loop to read the contents of the file and put them in the variable "contents" declared at the beginning of the method. */
-        } catch (/* TODO 
-Put the correct Exception here. */) {
+            while((lengthRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, lengthRead);
+                out.write('\n');
+                out.flush();
+            }
+        /* TODO
+Put the correct Exception here. */
+        } catch (FileNotFoundException e) {
+            System.out.println("No file found.");
+        } catch (IOException e) {
             e.printStackTrace();
             System.exit(-1);
         }
-        return/* TODO 
-Return a Databse object initialized with the contents read. */);
+
+        /* TODO
+Return a Database object initialized with the contents read. */
+        return new Database(contents);
     }
 }
