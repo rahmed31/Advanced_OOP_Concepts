6b38ecd08b676b3109f8b14990ed3a8c20b38c0a
Raihan Ahmed <raihan.ahmed117@gmail.com>
2022-03-15 00:36:18 -0500

    Completed JSONFiles module

--- FilesAndJson/JSONFiles/src/Database.java
+++ FilesAndJson/JSONFiles/src/Database.java
@@ -5,21 +5,49 @@ public class Database {
     /* TODO 
 Declare a member variable that can 
 be EASILY set from the constructor. */
+    JsonObject myReviewSet;
 
-    public Database(JsonObject data){
         /* TODO 
-set the memebr variable declared above.*/
+set the member variable declared above.*/
+    public Database(JsonObject data) {
+        myReviewSet = data;
     }
 
-    public JsonObject getRestaurant(String name){
-        /* TODO 
+        /* TODO
 Complete this method as specified. */
+    public JsonObject getRestaurant(String name) {
+        JsonArray restaurants = (JsonArray)myReviewSet.get("restaurants");
+
+        JsonObject myRestaurant = null;
+
+        for (int i = 0; i < restaurants.size(); i++) {
+            JsonObject restaurant = (JsonObject) restaurants.get(i);
+
+            if (restaurant.get("name").equals(name)) {
+                myRestaurant = restaurant;
+            }
+        }
+
+        return myRestaurant;
     }
 
-    public double getAvgReviews(String name){
         /* TODO 
 Complete this method as specified.
- The previous method may be of help. 
-*/
+ The previous method may be of help. */
+    public Double getAvgReviews(String name){
+        JsonObject myRestaurant = getRestaurant(name);
+
+        JsonArray myReviews = (JsonArray) myRestaurant.get("reviews");
+
+        double sum = 0;
+        double count = 0;
+
+        for (int i = 0; i < myReviews.size(); i++) {
+            JsonObject review = (JsonObject) myReviews.get(i);
+            sum += (double)review.get("rating");
+            count ++;
+        }
+
+        return sum/count;
     }
 }
--- FilesAndJson/JSONFiles/src/Task.java
+++ FilesAndJson/JSONFiles/src/Task.java
@@ -1,22 +1,49 @@
 import com.github.cliftonlabs.json_simple.JsonObject;
 import com.github.cliftonlabs.json_simple.Jsoner;
+import netscape.javascript.JSObject;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Task {
     public static void main(String[] args){
         Task t = new Task();
-        JsonObject doc = t.readJson("./resources/restaurant-data.json");
+        JsonObject doc = t.readJson("./test/restaurant-data.json");
         Database db = new Database(doc);
         System.out.println(db.getRestaurant("Hometown BBQ"));
         System.out.println(db.getAvgReviews("Casa Enrique"));
     }
 
-    public JsonObject readJson(String fi/* TODO: create a JSON object with the contents of  "filename". You can use the method below to help you read the file. */
-       /* Put in the variable that should be returned. */em.exit(-1);
+    public String readFile(String filename) {
+        String contents = "";
+
+        try (BufferedReader in = new BufferedReader(new FileReader(filename))) {
+            String line;
+
+            while((line = in.readLine()) != null) {
+                contents += line;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No file found.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
         }
-        return content;
+
+        System.out.println(contents);
+        return contents;
     }
+
+   /* TODO: create a JSON object with the contents of "filename".
+       You can use the method below to help you read the file. */
+       /* Put in the variable that should be returned. */
+
+    public JsonObject readJson(String filename) {
+
+        String json = readFile(filename);
+
+        JsonObject document = (JsonObject) Jsoner.deserialize(json, new JsonObject());
+
+        return document;
+    }
+
 }
