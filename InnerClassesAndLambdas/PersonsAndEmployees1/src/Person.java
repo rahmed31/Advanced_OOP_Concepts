6f4d045361d444075065c9b2cf03f56d638cb7e0
Raihan Ahmed <raihan.ahmed117@gmail.com>
2022-04-09 22:32:02 -0500

    Successfully completed PersonsAndEmployees1 module

--- InnerClassesAndLambdas/PersonsAndEmployees1/src/Person.java
+++ InnerClassesAndLambdas/PersonsAndEmployees1/src/Person.java
@@ -1,33 +1,75 @@
-/* TODO */
+/* TODO
 Import the java.time.LocalDate and java.time.Period classes.
-You will need them later.
+You will need them later. */
+import jdk.vm.ci.meta.Local;
 
-/* TODO */
-Declare the class
-    /* TODO */
-Create two private member variables to hold the name and the birthDate of the employee. The birthdate has to be a LocalDate
+import java.time.LocalDate;
+import java.time.Period;
 
-    /* TODO */
+/*TODO
+Declare the class*/
+public class Person {
+        /* TODO
+Create two private member variables to hold the name and the birthDate of the employee. The birthdate has to be a LocalDate */
+    private String name;
+    private LocalDate birthDate;
+    private Position position;
+    /* TODO
 Create a member inner class called Position with two
 member variables: a name and a salary. Override the toString()
-method as specified in the instructions
+method as specified in the instructions */
 
-    /* TODO */
-Create a member variable (of Person) of type Position.
+    private class Position {
+        protected String name;
+        protected double salary;
+
+        public Position(String name, double salary) {
+            this.name = name;
+            this.salary = salary;
+        }
+
+        @Override
+        public String toString() {
+            return name + ":" + salary;
+        }
+    }
+    /* TODO
+Create a member variable (of Person) of type Position.*/
 
 
-    /* TODO */
-Create a constructor for Person that takes in two Strings. A name and a birthdate in ISO format (yyyy-mm-dd) and sets the corresponding memeber variables correctly
+    /* TODO
+Create a constructor for Person that takes in two Strings. A name and a birthdate in ISO format (yyyy-mm-dd) and sets the corresponding member variables correctly*/
+    public Person(String name, String birthdate) {
+        this.name = name;
+        this.birthDate = LocalDate.parse(birthdate);
+    }
 
-    /* TODO */
-Create a getAge method that returns the age in years of this person. You need to use the Period class here.
-    /* TODO */
-Code the setPosition method. This should take one String and one double for the title and the salary respectively.
+    /* TODO
+Create a getAge method that returns the age in years of this person. You need to use the Period class here. */
+    /* TODO
+Code the setPosition method. This should take one String and one double for the title and the salary respectively. */
+    public int getAge() {
+        return Period.between(this.birthDate, LocalDate.now()).getYears();
+    }
 
-    /* TODO */
-A getter for position.
+    public void setPosition(String title, double salary) {
+        this.position = new Position(title, salary);
+    }
 
-    /* TODO */
-Override the toString() method to display the person's name, age and position (with salary and title).
-/* TODO */
-Finish closing curly braces and the class.
+    /* TODO
+A getter for position. */
+    public String getPosition() {
+        return position.toString();
+    }
+
+    /* TODO
+Override the toString() method to display the person's name, age and position (with salary and title). */
+/* TODO
+Finish closing curly braces and the class.*/
+
+    @Override
+    public String toString() {
+        return "Name: " + this.name + "\nAge: " + this.getAge() + "\n" + this.getPosition();
+    }
+
+}
